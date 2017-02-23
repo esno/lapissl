5316253fadb156dad9877413476cc6577b790d19
crito <crito@fnordpipe.org>
2017-02-24 00:19:35 +0100

    add default config

--- src/app.lua
+++ src/app.lua
@@ -1,10 +1,36 @@
 local lapis = require("lapis")
+local json = require("json")
+
 local app = lapis.Application()
 
-app:get("/v1/status", function(self)
-  self.status = { status = "OK" }
+local config = {
+  data = "./data",
+  profiles = {
+    rootca = {
+      basic_constraints = {
+        critical = true,
+        ca = true,
+        pathlen = 0
+      },
+      key_usage = {
+        "Certificate Signer",
+        "CRL Signer"
+      },
+      expiry = "50y"
+    }
+  }
+}
 
-  return { status = 200, json = self.status }
+app:get("/v1/status", function(self)
+  status = { status = "OK" }
+
+  return { status = 200, json = status }
+end)
+
+app:get("/v1/x509/cert", function(self)
+  response = { out = config.data }
+
+  return { status = 200, json = response }
 end)
 
 return app
