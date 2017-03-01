7109124e37cb4269a6737d71df53f351a759c3e3
crito <crito@fnordpipe.org>
2017-03-01 19:22:01 +0100

    api: add condition on context path

--- src/api/init.lua
+++ src/api/init.lua
@@ -14,19 +14,23 @@ function api.init(self)
   for version, blueprint in pairs(api.version) do
     for endpoint, definition in pairs(blueprint) do
       if definition.get then
-        for k, context in pairs(definition.get) do
-          api.app:get(
-            "/" .. version .. "/" .. endpoint .. context.context,
-            json_params(function(self) return context:callable(self.params) end)
-          )
+        for _, ctx in pairs(definition.get) do
+          if ctx.context then
+            api.app:get(
+              "/" .. version .. "/" .. endpoint .. ctx.context,
+              json_params(function(self) return ctx:callable(self.params) end)
+            )
+          end
         end
       end
       if definition.post then
-        for k, context in pairs(definition.post) do
-          api.app:post(
-            "/" .. version .. "/" .. endpoint .. context.context,
-            json_params(function(self) return context:callable(self.params) end)
-          )
+        for _, ctx in pairs(definition.post) do
+          if ctx.context then
+            api.app:post(
+              "/" .. version .. "/" .. endpoint .. ctx.context,
+              json_params(function(self) return ctx:callable(self.params) end)
+            )
+          end
         end
       end
     end
